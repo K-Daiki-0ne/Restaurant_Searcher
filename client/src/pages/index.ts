24880f6e1381bc5db4b33a8817dec22f95efaa59
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-13 21:19:36 +0900

    creaet: pages component one together

---
+++ client/src/pages/index.ts
@@ -0,0 +1,6 @@
+export { Home } from './Home/Home';
+export { List } from './List/List';
+export { Detail } from './Detail/Detail';
+export { Login } from './Login/Login';
+export { Register } from './Register/Register';
+export { Error } from './Error/Error';
