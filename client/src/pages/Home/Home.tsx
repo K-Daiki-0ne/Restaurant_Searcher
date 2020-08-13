664f8fec5f481661274313e17054e48cb1595065
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-13 22:33:27 +0900

    add: connection HomeComponent to pages/Home

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Home.css';
+import { HomeComponent } from '../../components/templates/index';
 
 export function Home() {
   return (
     <div className='home'>
-      <h1>This page is Home</h1>
+      <HomeComponent />
     </div>
   )
 }
