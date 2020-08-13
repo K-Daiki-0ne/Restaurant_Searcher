ed5a28fe3e55054a9c6290dbe169265648e7ac48
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-13 23:26:34 +0900

    create: CatchPhraseText Design & responsive design

--- client/src/components/atoms/CatchPhraseText/CatchPhraseText.tsx
+++ client/src/components/atoms/CatchPhraseText/CatchPhraseText.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import './CatchPhraseText.css'
 
 export function CatchPhraseText() {
   return (
-    <div>
+    <div className='catchphrase'>
       <h1>いま何がたべたいですか？</h1>
     </div>
   )
