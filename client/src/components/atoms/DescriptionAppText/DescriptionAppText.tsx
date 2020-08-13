458c47f90375ad21c432796f86b181c8d88b8c10
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-13 23:45:04 +0900

    create: DescriptionAppText component & design

--- client/src/components/atoms/DescriptionAppText/DescriptionAppText.tsx
+++ client/src/components/atoms/DescriptionAppText/DescriptionAppText.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import './DescriptionAppText.css';
 
 export function DescriptionAppText() {
   return (
-    <div>
+    <div className='descriptiontext'>
       <h3>RestaurantSearcherはあなたの周囲の飲食店を検索します。</h3>
     </div>
   )
