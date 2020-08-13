e187ae0576d11e0454b9cabba2ba1b2b54daa09a
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-13 22:09:18 +0900

    create: pages/Home design

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import './Home.css';
 
 export function Home() {
   return (
-    <h1>This page is Home</h1>
+    <div className='home'>
+      <h1>This page is Home</h1>
+    </div>
   )
 }
