ed9f371e82d48ff930403ba0622738a237b68c06
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-13 22:29:37 +0900

    create: HomeComponent

---
+++ client/src/components/templates/HomeComponent/HomeComponent.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  CatchPhraseText,
+  DescriptionAppText
+} from '../../atoms/index';
+
+export function HomeComponent() {
+  return (
+    <div>
+      <CatchPhraseText />
+      <DescriptionAppText />
+    </div>
+  )
+}
