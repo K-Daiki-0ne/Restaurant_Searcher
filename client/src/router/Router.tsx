285a369d59760698d267df71d335d748adf65c5b
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-13 21:25:06 +0900

    create: front router

---
+++ client/src/router/Router.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import {
+  Home,
+  List,
+  Detail,
+  Login,
+  Register,
+  Error
+} from '../pages/index';
+
+export function Router() {
+  return (
+    <BrowserRouter>
+      <Route exact path='/' component={Home} />
+      <Route path='/list' component={List} />
+      <Route path='/detail' component={Detail} />
+      <Route path='/login' component={Login} />
+      <Route path='/register' component={Register} />
+      <Route path='/error' component={Error} />
+    </BrowserRouter>
+  )
+}
