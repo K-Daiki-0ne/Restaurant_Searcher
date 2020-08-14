45b9262505b476a66bf8deaea32a7c39a654a68d
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-14 22:49:47 +0900

    add: connection Footer Component

--- client/src/components/organisms/AppFooter/AppFooter.tsx
+++ client/src/components/organisms/AppFooter/AppFooter.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './AppFooter.css';
+import {
+  FooterAuthorText,
+  FooterApiText,
+  FooterImageUrlText
+} from '../../atoms/index';
 
 export function AppFooter() {
   return (
     <footer className='footer'>
-
+      <FooterAuthorText />
+      <FooterApiText />
+      <FooterImageUrlText />
     </footer>
   )
 }
