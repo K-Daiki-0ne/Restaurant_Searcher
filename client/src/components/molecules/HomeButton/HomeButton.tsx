003bbd487a127e1edb83a18938fa32c4e50866ac
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-14 22:14:03 +0900

    delete: text border

--- client/src/components/molecules/HomeButton/HomeButton.tsx
+++ client/src/components/molecules/HomeButton/HomeButton.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { HeaderTitleText } from '../../atoms/index';
 import { Link } from 'react-router-dom'
-
+import './HomeButton.css';
 
 export const HomeButton: React.FC = () => {
   return (
     <div>
-      <Link to='/'>
+      <Link to='/' className='link'>
         <HeaderTitleText />
       </Link>
     </div>
