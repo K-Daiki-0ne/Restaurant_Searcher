1d3dae6e1f342131e9e5ae6932c3da784303073c
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-14 23:24:55 +0900

    fix: FooterAuthorText text

--- client/src/components/atoms/FooterAuthorText/FooterAuthorText.tsx
+++ client/src/components/atoms/FooterAuthorText/FooterAuthorText.tsx
@@ -4,8 +4,8 @@ import './FooterAuthorText.css'
 export function FooterAuthorText() {
   return (
     <h4 className='author-text'>
-      Create: K_Daiki,&nbsp;&nbsp;&nbsp;&nbsp;
-      <a href="https://github.com/K-Daiki-0ne/Restaurant_Searcher" className='link'>Code</a>
+      Create: &nbsp;K_Daiki,&nbsp;&nbsp;&nbsp;&nbsp;
+      <a href="https://github.com/K-Daiki-0ne/Restaurant_Searcher" className='link'>SourceCode</a>
     </h4>
   )
 }
