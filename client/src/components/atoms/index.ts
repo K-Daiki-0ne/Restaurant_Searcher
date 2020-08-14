31b87c1e640133bef50c6e9dfd5e90987526c13e
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-14 19:27:40 +0900

    add: component one together

--- client/src/components/atoms/index.ts
+++ client/src/components/atoms/index.ts
@@ -1,3 +1,6 @@
 export { CatchPhraseText } from './CatchPhraseText/CatchPhraseText';
 export { DescriptionAppText } from './DescriptionAppText/DescriptionAppText';
 export { HeaderTitleText } from './HeaderTitleText/HeaderTitleText';
+export { FooterApiText } from './FooterApiText/FooterApiText';
+export { FooterAuthorText } from './FooterAuthorText/FooterAuthorText';
+export { FooterImageUrlText } from './FooterImageUrlText/FooterImageUrlText';
