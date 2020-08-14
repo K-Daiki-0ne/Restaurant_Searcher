5bdf34002552dd90a68eaa52d5660e78df406e0f
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-15 00:01:38 +0900

    create: FooterImageUrlText component

--- client/src/components/atoms/FooterImageUrlText/FooterImageUrlText.tsx
+++ client/src/components/atoms/FooterImageUrlText/FooterImageUrlText.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import './FooterImageUrlText.css';
 
 export function FooterImageUrlText() {
   return (
-    <div>
-      <h4>
+    <div className='imege-text'>
+      <h4 className='image-url'>
         使用画像　https://www.foodiesfeed.com/free-food-photo/messy-pizza-on-a-black-table/
       </h4>
     </div>
