f595eddb49ab62fbcbc79da57580e5f0eadcb0a4
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-14 22:28:11 +0900

    add: connect AppFooter to App

--- client/src/root/App.tsx
+++ client/src/root/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Router } from '../router/Router';
+import { AppFooter } from '../components/organisms/index';
 import './App.css';
 
 function App() {
   return (
     <div className='app'>
       <Router />
+      <AppFooter />
     </div>
   )
 }
