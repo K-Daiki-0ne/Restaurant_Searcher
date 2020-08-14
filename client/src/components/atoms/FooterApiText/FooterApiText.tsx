59ee597fce3fc24a69237460d38c597bf18ec933
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-14 23:48:54 +0900

    create: FooterApiText design

--- client/src/components/atoms/FooterApiText/FooterApiText.tsx
+++ client/src/components/atoms/FooterApiText/FooterApiText.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import './FooterApiText.css';
 
 export function FooterApiText() {
   return (
-    <div>
-      <h4>
+    <div className='api-text'>
+      <h4 className='api'>
         使用するAPIについて記述する
       </h4>
     </div>
