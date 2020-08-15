cfbf602dab1fd029611b5bc53d10911b1ddaad07
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-15 22:15:08 +0900

    create: SearchButton design

--- client/src/components/molecules/SearchButton/SearchButton.tsx
+++ client/src/components/molecules/SearchButton/SearchButton.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { SeaarchIcon } from '../../atoms/index';
+import './SearchButton.css'
 
 export function SearchButton() {
   return (
-    <div>
-      <Link to={`/detail`}>
-        <button>
+    <div className='search'>
+      <Link to={`/detail`} className='link'>
+        <button className='search-button'>
           <SeaarchIcon />
         </button>
       </Link>
