b0164ff429774881d69866c085988af0b0c9de8e
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-15 15:13:53 +0900

    add: connect HeaderLoginText to LoginButton

--- client/src/components/molecules/LoginButton/LoginButton.tsx
+++ client/src/components/molecules/LoginButton/LoginButton.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { HeaderLoginText } from '../../atoms/index';
 
 export function DetailRestaurantAdressText() {
   return (
     <div>
       <Link to='/login'>
-        {/*  */}
+        <HeaderLoginText />
       </Link>
     </div>
   )
