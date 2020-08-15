91c533ef734980aa3a0194b7b8fd917c6f59142f
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-15 21:25:56 +0900

    create: ModalDescriptionText & desing

--- client/src/components/atoms/ModalDescriptionText/ModalDescriptionText.tsx
+++ client/src/components/atoms/ModalDescriptionText/ModalDescriptionText.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
+import './ModalDescriptionText.css';
 
 export function ModalDescriptionText() {
   return (
-    <div>
-      <h1>RestrauntSearcherについて</h1>
-
-      <h3>検索ボタンをクリックするとあなたの周辺の飲食店を検索します。</h3>
-
-      <p>ユーザー登録をすることで、お気に入りのお店を登録することができます。</p><br />
-
-      <p>ユーザー登録をしたい場合はLoginをクリックしてください！</p><br />
-
-      <p>RestrauntSearcherは無料のサービスです。また、ユーザー登録をしなくても周囲の飲食店を検索することができます。</p>
+    <div className='description-text'>
+      <h1 className='app-guide'>RestrauntSearcherについて</h1>
+      <h2 className='app-explanation'>RestrauntSearcherはあなたの周囲の飲食店を探してくれます。</h2>
+      <h3 className='app-sub-explanation'>検索ボタンをクリックするとあなたの周辺の飲食店を検索します。</h3>
+      <div className='app-text'>
+        <p>ユーザー登録をすることで、お気に入りのお店を登録することができます。</p><br />
+        <p>ユーザー登録をしたい場合はLoginをクリックしてください！</p><br />
+        <p>RestrauntSearcherは無料のサービスです。また、ユーザー登録をしなくても周囲の飲食店を検索することができます。</p>
+      </div>
     </div>
   )
 }
