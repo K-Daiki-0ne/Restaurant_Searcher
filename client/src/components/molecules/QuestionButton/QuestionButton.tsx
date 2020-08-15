59e7a1fb28900ee0deddf381bb220be8f80b73ef
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-15 16:57:57 +0900

    create: QuestionButton component

--- client/src/components/molecules/QuestionButton/QuestionButton.tsx
+++ client/src/components/molecules/QuestionButton/QuestionButton.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { HeaderQuestionIcon } from '../../atoms/index';
+import Modal from 'react-modal';
 
-export function QuestionButton() {
+export const QuestionButton: React.FC = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => {
+    setIsOpen(true);
+  }
+
+  const closeModal = () => {
+    setIsOpen(false);
+  }
+
   return (
     <div>
-      <HeaderQuestionIcon />
+      <button onClick={openModal}>
+        <HeaderQuestionIcon />
+      </button>
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={closeModal}
+        contentLabel='RestrauntSearcherについて'
+      >
+        <button onClick={closeModal}>
+          Close
+        </button>
+        <p>
+          サンプル
+        </p>
+      </Modal>
     </div>
   )
 }
