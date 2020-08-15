e34dc1bdeb8b5596038f8bf0b9767e2f65b7f290
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-15 22:02:14 +0900

    add: icon size

--- client/src/components/atoms/SearchIcon/SearchIcon.tsx
+++ client/src/components/atoms/SearchIcon/SearchIcon.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import './SearchIcon.css';
 
 export function SeaarchIcon() {
   return (
-    <div>
+    <div className='search-icon'>
       <i className="fas fa-search"></i>
     </div>
   )
