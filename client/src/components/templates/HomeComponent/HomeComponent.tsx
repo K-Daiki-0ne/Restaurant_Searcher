a76eccb9a023c0beddf550c8a66416ffe4c59242
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-15 21:56:58 +0900

    add: connect SearchButton to HomeComponent

--- client/src/components/templates/HomeComponent/HomeComponent.tsx
+++ client/src/components/templates/HomeComponent/HomeComponent.tsx
@@ -3,12 +3,14 @@ import {
   CatchPhraseText,
   DescriptionAppText
 } from '../../atoms/index';
+import { SearchButton } from '../../molecules/index';
 
 export function HomeComponent() {
   return (
     <div>
       <CatchPhraseText />
       <DescriptionAppText />
+      <SearchButton />
     </div>
   )
 }
