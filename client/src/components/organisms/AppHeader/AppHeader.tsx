bff1e513e0f3309b14f733f803f58db4ed6e6917
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-15 17:01:49 +0900

    add: connect QuestionButton to AppHeader

--- client/src/components/organisms/AppHeader/AppHeader.tsx
+++ client/src/components/organisms/AppHeader/AppHeader.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 import './AppHeader.css';
 import { 
   HomeButton,
-  LoginButton 
+  LoginButton,
+  QuestionButton
 } from '../../molecules/index';
 
 export function AppHeaer() {
   return (
     <header className='header'>
+      <nav className='question-button'>
+        <QuestionButton />
+      </nav>
       <div className='main-text'>
         <HomeButton />
       </div>
