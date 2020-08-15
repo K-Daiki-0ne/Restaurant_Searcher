fdc952b201f24a48be67c278df09959a7b8b70e5
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-15 18:06:20 +0900

    create: ModalDescriptionText component

---
+++ client/src/components/atoms/ModalDescriptionText/ModalDescriptionText.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export function ModalDescriptionText() {
+  return (
+    <div>
+      <h1>RestrauntSearcherについて</h1>
+
+      <h3>検索ボタンをクリックするとあなたの周辺の飲食店を検索します。</h3>
+
+      <p>ユーザー登録をすることで、お気に入りのお店を登録することができます。</p><br />
+
+      <p>ユーザー登録をしたい場合はLoginをクリックしてください！</p><br />
+
+      <p>RestrauntSearcherは無料のサービスです。また、ユーザー登録をしなくても周囲の飲食店を検索することができます。</p>
+    </div>
+  )
+}
