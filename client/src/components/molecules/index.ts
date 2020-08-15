05146348df2f826c7db92038c7db5466db4ebcd8
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-15 21:52:07 +0900

    add: component one together

--- client/src/components/molecules/index.ts
+++ client/src/components/molecules/index.ts
@@ -1,3 +1,4 @@
 export { HomeButton } from './HomeButton/HomeButton';
 export { LoginButton } from './LoginButton/LoginButton'; 
 export { QuestionButton } from './QuestionButton/QuestionButton';
+export { SearchButton } from './SearchButton/SearchButton';
