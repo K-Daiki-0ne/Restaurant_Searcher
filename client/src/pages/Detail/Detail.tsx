2750bf8867a820431d53f9aa2ec675acf598fb86
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-15 22:24:08 +0900

    create: base pages/Detail component

--- client/src/pages/Detail/Detail.tsx
+++ client/src/pages/Detail/Detail.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { AppHeaer } from '../../components/organisms/index';
+import './Detail.css';
 
 export function Detail() {
   return (
-    <h1>This page is Detail</h1>
+    <div className='detail'>
+      <AppHeaer />
+      <div className='detail-content'>
+        {/* DetailRestrauntCard */}
+      </div>
+    </div>
   )
 }
