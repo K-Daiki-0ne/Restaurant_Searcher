294f40fa0e13e4ade9375b020c5e932a32b005cc
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-15 23:50:11 +0900

    fix: text

--- client/src/components/templates/DetailRestaurantCard/DetailRestaurantCard.tsx
+++ client/src/components/templates/DetailRestaurantCard/DetailRestaurantCard.tsx
@@ -5,17 +5,17 @@ export const DetailRestrauntCard: React.FC = () => {
   return (
   <div className="grid">
     <article className="restraunt-image">
-      <p>sample</p>
+      <p>お店の画像</p>
     </article>
     <article className="restraunt-content">
       <section className='name'>
         <p>
-          sample
+          お店の名前
         </p>
       </section>
       <section className='adress'>
         <p>
-          sample02
+          お店の住所
         </p>
       </section>
     </article>
