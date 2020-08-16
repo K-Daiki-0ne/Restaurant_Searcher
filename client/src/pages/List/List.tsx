f2657d1f3aa56be78a3e45006602beaf652ed80d
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-16 21:31:37 +0900

    create: pages/Login design

--- client/src/pages/List/List.tsx
+++ client/src/pages/List/List.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { AppHeaer } from '../../components/organisms/index';
+import { ListRestaurantCard } from '../../components/templates/index';
+import './List.css';
 
 export function List() {
   return (
-    <h1>This page is List</h1>
+    <div className='list'>
+      <AppHeaer />
+      <div className='content'>
+        <ListRestaurantCard />
+      </div>
+    </div>
   )
 }
