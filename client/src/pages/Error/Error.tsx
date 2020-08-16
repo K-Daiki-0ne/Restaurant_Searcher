be90071321324e4142aae161282b54c8a65460e3
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-16 23:17:44 +0900

    create: Erorr page design

--- client/src/pages/Error/Error.tsx
+++ client/src/pages/Error/Error.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { AppHeaer } from '../../components/organisms/index';
+import {
+  ErrorIcon,
+  ErrorText
+} from '../../components/atoms/index';
+import './Error.css'
 
 export function Error() {
   return (
-    <h1>This page is Error</h1>
+    <div className='error'>
+      <AppHeaer />
+      <div className='error-content'>
+        <div>
+          <ErrorIcon />
+        </div>
+        <div>
+          <ErrorText />
+        </div>
+      </div>
+    </div>
   )
 }
