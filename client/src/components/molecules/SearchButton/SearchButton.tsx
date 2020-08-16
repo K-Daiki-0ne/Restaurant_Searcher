171674f199119dedc7febf8a4e29367f24a4fb95
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-16 21:25:45 +0900

    fix: route path

--- client/src/components/molecules/SearchButton/SearchButton.tsx
+++ client/src/components/molecules/SearchButton/SearchButton.tsx
@@ -6,7 +6,7 @@ import './SearchButton.css'
 export function SearchButton() {
   return (
     <div className='search'>
-      <Link to={`/detail`} className='link'>
+      <Link to={`/list`} className='link'>
         <button className='search-button'>
           <SeaarchIcon />
         </button>
