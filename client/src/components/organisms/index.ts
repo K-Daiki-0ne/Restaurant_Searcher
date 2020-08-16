1452fb9e646d1b7bed10342acaceb2897cfa16dc
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-16 22:23:22 +0900

    add: component one together

--- client/src/components/organisms/index.ts
+++ client/src/components/organisms/index.ts
@@ -1,3 +1,5 @@
 export { AppHeaer } from './AppHeader/AppHeader';
 export { AppFooter } from './AppFooter/AppFooter';
 export { ListTitle } from './ListTitle/ListTitle';
+export { ListImageCard } from './ListImageCard/ListImageCard';
+export { ListAdressCard } from './ListAdressCard/ListAdressCard';
