8b7c5f80398b526fa238c83899da20a49f8b8273
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-16 22:25:09 +0900

    add: connect component

--- client/src/components/templates/ListRestaurantCard/ListRestaurantCard.tsx
+++ client/src/components/templates/ListRestaurantCard/ListRestaurantCard.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { ListTitle } from '../../organisms/index';
+import { 
+  ListTitle,
+  ListAdressCard,
+  ListImageCard 
+} from '../../organisms/index';
 import './ListRestaurantCard.css';
 
 export const ListRestaurantCard: React.FC = () => {
   return (
   <div className="grid">
     <article className="restraunt-image">
-      <p>sample</p>
+      <ListImageCard />
     </article>
     <article className="restraunt-content">
       <section className='name'>
         <ListTitle />
       </section>
       <section className='adress'>
-        <Link to='/detail'>
-          <p>
-            お店の住所
-          </p>
-        </Link>
+        <ListAdressCard />
       </section>
     </article>
   </div>
