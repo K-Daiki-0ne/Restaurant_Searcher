02a1fc27d9ca576d2697fbd3996dc6c44cf4c338
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-16 21:01:19 +0900

    create: base LoginDescriptionText component

--- client/src/components/atoms/LoginDescriptionText/LoginDescriptionText.tsx
+++ client/src/components/atoms/LoginDescriptionText/LoginDescriptionText.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export function DetailRestaurantAdressText() {
+export function LoginDescriptionText() {
   return (
-    <p>DetailRestaurantAdressText</p>
+    <div>
+      <h3>RestaurantSearcherにログインすることで気に入ったお店をお気に入りに登録することができます</h3>
+    </div>
   )
 }
