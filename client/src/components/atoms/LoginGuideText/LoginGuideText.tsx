64b5c2fd62148a255a7fd26a4ef8cb5483111aa8
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-16 21:03:37 +0900

    create: base LoginGuideText component

--- client/src/components/atoms/LoginGuideText/LoginGuideText.tsx
+++ client/src/components/atoms/LoginGuideText/LoginGuideText.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-export function DetailRestaurantAdressText() {
+export function LoginGuideText() {
   return (
-    <p>DetailRestaurantAdressText</p>
+    <div>
+      <h3>
+        まだお気に入り登録をしていない人は<Link to='/register'>こちら</Link>から
+      </h3>
+    </div>
   )
 }
