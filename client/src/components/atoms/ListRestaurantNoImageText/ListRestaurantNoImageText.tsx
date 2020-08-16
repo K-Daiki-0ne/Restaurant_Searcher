92dfdaca2bd33c2b8b6294175d96572e33b3aa6d
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-16 22:29:50 +0900

    create: ListRestaurantNoImage component

--- client/src/components/atoms/ListRestaurantNoImageText/ListRestaurantNoImageText.tsx
+++ client/src/components/atoms/ListRestaurantNoImageText/ListRestaurantNoImageText.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
-export function DetailRestaurantAdressText() {
+export const ListRestaurantNoImageText = () => {
   return (
-    <p>DetailRestaurantAdressText</p>
+    <p>No Image</p>
   )
 }
