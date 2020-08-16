a1217df609da68478a5f87403c9afc599fba517c
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-16 22:22:09 +0900

    create: ListAdressCard component

--- client/src/components/organisms/ListAdressCard/ListAdressCard.tsx
+++ client/src/components/organisms/ListAdressCard/ListAdressCard.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-export function DetailRestaurantAdressText() {
+export const ListAdressCard = () => {
   return (
-    <p>DetailRestaurantAdressText</p>
+    <div>
+      <Link to='/detail'>
+        <p>お店の住所</p>
+      </Link>
+    </div>
   )
 }
