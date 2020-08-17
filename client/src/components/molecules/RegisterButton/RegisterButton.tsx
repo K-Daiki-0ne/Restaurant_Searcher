6ea25c0ea4082b0c0f057b161b47d7020b4aebb1
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-17 23:34:16 +0900

    create: RegisterButton design

--- client/src/components/molecules/RegisterButton/RegisterButton.tsx
+++ client/src/components/molecules/RegisterButton/RegisterButton.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { RegisterButtonText } from '../../atoms/index';
+import './RegisterButton.css';
 
 export function RegisterButton() {
   return (
-    <button>
+    <button className='register-button'>
       <RegisterButtonText />
     </button>
   )
