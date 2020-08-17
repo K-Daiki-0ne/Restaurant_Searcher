94d395a242c8f93a79b7f3820575f8f1fa8c2622
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-17 23:07:50 +0900

    add: component one together

--- client/src/components/templates/index.ts
+++ client/src/components/templates/index.ts
@@ -2,3 +2,4 @@ export { HomeComponent } from './HomeComponent/HomeComponent';
 export { DetailRestrauntCard } from './DetailRestaurantCard/DetailRestaurantCard';
 export { ListRestaurantCard } from './ListRestaurantCard/ListRestaurantCard';
 export { LoginUserForm } from './LoginUserForm/LoginUserForm';
+export { RegisterUserForm } from './RegisterUserForm/RegisterUserForm';
