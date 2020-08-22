2755f69a92fd0cf0dc99fd799a7071c19a41a810
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-22 21:43:49 +0900

    delete: adress route

--- server/routes/web.php
+++ server/routes/web.php
@@ -16,7 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'api/v1'], function () {
     Route::get('{lat}/{lon}', 'RestaurantController@index')->name('restaurant.index');
     Route::get('{id}', 'RestaurantController@detail')->name('restaurant.detail');
-    Route::get('adress/{adress}', 'RestaurantController@index')->name('restaurant.adress');
 });
 
 Route::group(['prefix' => 'login'], function () {
