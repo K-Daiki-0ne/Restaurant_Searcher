eb62d11caf0f59dc0592d4d1c6c5609b651e7e72
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-22 21:26:05 +0900

    add: detail restaurant request data function

--- server/app/Services/GetApiData.php
+++ server/app/Services/GetApiData.php
@@ -11,6 +11,13 @@ class GetApiData
         $url = "{$base_url}&latitude={$lat}&longitude={$lon}";
         $result = ApiRequest::request($url);
         return $result;
-	}
+    }
+    
+    public static function getDetailRestaurant($id) {
+        $base_url = Url::baseUrl();
+        $url = "{$base_url}&id={$id}";
+        $result = ApiRequest::request($url);
+        return $result;
+    }
 }
 ?>
