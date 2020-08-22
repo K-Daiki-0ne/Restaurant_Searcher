3a2b31f321548539723ea3636efcf40ef785e2fe
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-22 13:00:07 +0900

    create: division api request

---
+++ server/app/API/ApiRequest.php
@@ -0,0 +1,24 @@
+<?php
+class ApiRequest
+{
+	public static function request($url) {
+		try {
+			$ch = curl_init();
+  
+			curl_setopt($ch, CURLOPT_URL, $url);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
+  
+		  	$response = curl_exec($ch);
+  
+		  	$result = json_decode($response, true);
+  
+		  	return $result;
+		} catch(Exception $ex) {
+			$result = $ex;
+			return $result;
+		}
+  }
+}
+
+?>
