57340929ac86f7280ad8d943a0ffa75a7efbc685
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-23 14:34:57 +0900

    add: try catch

--- server/app/Http/Controllers/RegisterController.php
+++ server/app/Http/Controllers/RegisterController.php
@@ -3,10 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\UserForm;
 
 class RegisterController extends Controller
 {
     public function index($name, $password) {
+        $request = new UserForm();
+        // $request->$name = $name;
+        // $request->password = $password;
+
+        // $request->save();
+        try {
+            $request->$name = $name;
+            $request->password = $password;
+
+            $request->save();
+        
+            $success = 'ユーザーの作成に成功しました';
+            return response() -> json($success);
+        } catch (Exception $err) {
+            $error = $err;
+            return response() -> json($error);
+        }
 
     }
 }
