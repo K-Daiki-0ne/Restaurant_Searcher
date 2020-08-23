5b432de340e262ac0521a93a561dce89277f0732
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-23 22:30:13 +0900

    create: user login controller

--- server/app/Http/Controllers/LoginController.php
+++ server/app/Http/Controllers/LoginController.php
@@ -4,7 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\UserForm;
+use App\Models\UserFavoriteRestaurant;
+
+
 class LoginController extends Controller
 {
-    //
+    public function index($user_name, $user_password) {
+
+        $name = UserForm::where('name', $user_name)
+            ->where('password', $user_password)
+            ->first();
+
+        return response() -> json($name);
+    }
 }
