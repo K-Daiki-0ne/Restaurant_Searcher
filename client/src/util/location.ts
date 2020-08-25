a8e689abb120e0fe2bfe92130b9d8e975771a06f
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-25 23:20:46 +0900

    create: get user current position function

---
+++ client/src/util/location.ts
@@ -0,0 +1,31 @@
+export const getLocation = () => {
+  if (navigator.geolocation) {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(
+        pos => {
+          resolve({
+            lat: pos.coords.latitude, 
+            lon: pos.coords.longitude 
+          })
+        },
+        err => {
+          switch(err.code) {
+            case 1:
+              alert('This terminal can not allow get current position')
+              break
+            case 2:
+              alert('Sorry, failed to get position')
+              break
+            case 3:
+              alert('Sorry, timeout')
+              break
+            default:
+              break
+          }
+        },
+      )
+    })
+  } else {
+    alert('Sorry, this terminal can not use geolocation');
+  }
+}
