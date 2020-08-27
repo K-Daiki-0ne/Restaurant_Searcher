2cbc31452599c853650e0ef68872d50e40888922
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-27 23:22:30 +0900

    move: get current postion function

--- client/src/components/molecules/SearchButton/SearchButton.tsx
+++ client/src/components/molecules/SearchButton/SearchButton.tsx
@@ -1,23 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { useSetRecoilState } from 'recoil';
-import { userCurrentLocation } from '../../../store/atoms/location';
 import { SeaarchIcon } from '../../atoms/index';
-import { getLocation } from '../../../util/location';
 import './SearchButton.css'
 
 
 export function SearchButton() {
-  const setUserCurrentLocation = useSetRecoilState(userCurrentLocation);
-
-  useEffect(() => {
-    getLocation()
-      .then((data: object | any) => setUserCurrentLocation(data));
-  }, []);
-
-
-
-  return (
+    return (
     <div className='search'>
       <Link to={`/list`} className='link'>
         <button className='search-button' >
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useRecoilState } from 'recoil';
+import { userCurrentLocation } from '../../store/atoms/location';
+import { getLocation } from '../../util/location';
 import './Home.css';
 import { HomeComponent } from '../../components/templates/index';
 import { AppHeaer } from '../../components/organisms/AppHeader/AppHeader';
 
+
 export function Home() {
+  const [, setUserCurrentLocation] = useRecoilState(userCurrentLocation);
+
+  useEffect(() => {
+    getLocation()
+      .then((data: object | any) => setUserCurrentLocation(data));
+  }, []);
   return (
     <div className='home'>
       <AppHeaer />
