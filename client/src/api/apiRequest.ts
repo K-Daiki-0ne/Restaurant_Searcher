0a0375d86154bb047ee82542fbec421930c0e01d
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-28 23:12:25 +0900

    fix: api request

--- client/src/api/apiRequest.ts
+++ client/src/api/apiRequest.ts
@@ -5,10 +5,11 @@ export const apiRequest = async (lat: string, lon: string) => {
     const baseUrl = process.env.REACT_APP_BACKEND_URL;
     const reqURL = `${baseUrl}/api/v1/${lat}/${lon}`;
     const data = await axios.get(reqURL);
-    return data    
+    return data.data.rest    
   } catch (error) {
+    const response = 'データの取得に失敗しました';
     return {
-      error
+      response
     }
   }
 }
