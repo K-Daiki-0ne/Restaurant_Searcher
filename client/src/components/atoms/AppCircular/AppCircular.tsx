68cfa96e8eaaf2a45a71ffabf084902169493ef7
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-28 23:47:00 +0900

    create: AppCircular

--- client/src/components/atoms/AppCircular/AppCircular.tsx
+++ client/src/components/atoms/AppCircular/AppCircular.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import './AppCircular.css';
 
 export function AppCircular() {
-  <div className='spin'></div>
+  <div className='spin'>
+    <ul>
+      <li>L</li>
+      <li>O</li>
+      <li>A</li>
+      <li>D</li>
+      <li>I</li>
+      <li>N</li>
+      <li>G</li>
+  </ul>
+  </div>
 }
