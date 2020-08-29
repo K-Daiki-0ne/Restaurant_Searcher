6a70d009c697f9b2624b9e3ec264d1e1693cfa7e
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-29 17:08:21 +0900

    add: register user password

--- client/src/store/atoms/register.ts
+++ client/src/store/atoms/register.ts
@@ -2,7 +2,14 @@ import { atom } from 'recoil';
 
 const registerName: string = '';
 
+const registerPassword: string = '';
+
 export const registerUserName = atom({
   key: 'RegisterUser',
   default: registerName
 });
+
+export const registerUserPassword = atom({
+  key: 'RegisterPassword',
+  default: registerPassword
+})
