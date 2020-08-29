8cf025b262408dd4d878eeab1c035bf66a274de0
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-29 17:40:26 +0900

    add: store register user password

--- client/src/components/molecules/RegisterPasswordInputForm/RegisterPasswordInputForm.tsx
+++ client/src/components/molecules/RegisterPasswordInputForm/RegisterPasswordInputForm.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import { useRecoilState } from 'recoil';
+import { registerUserPassword } from '../../../store/atoms/register'
 import './RegisterPasswordInputForm.css';
 
 export function RegisterPasswordInputForm() {
+  const [registerName, setRegisterPassword] = useRecoilState(registerUserPassword);
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRegisterPassword(event.target.value);
+  }
+
   return (
     <div>
       <label className='register-pass-label'>Password</label>
       <input 
         type="password" 
         className="register-password"  
-        placeholder="パスワードを入力してください"  
+        placeholder="パスワードを入力してください"
+        value={registerName}
+        onChange={onChange}
         required
       />
     </div>
