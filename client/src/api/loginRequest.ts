8be278b3d07742857df067824fddf93d534d07bb
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-29 23:50:28 +0900

    fix: add try catch for loginRequest

--- client/src/api/loginRequest.ts
+++ client/src/api/loginRequest.ts
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
 export const loginRequest = async (name: string, password: string) => {
-  const baseUrl = process.env.REACT_APP_BACKEND_URL;
-  const reqUrl = `${baseUrl}/login/user/${name}/${password}`;
-  const result = await axios.get(reqUrl);
-  return result;
+  try {
+    const baseUrl = process.env.REACT_APP_BACKEND_URL;
+    const reqUrl = `${baseUrl}/login/user/${name}/${password}`;
+    const result = await axios.get(reqUrl);
+    return result;
+  } catch (err) {
+    return 'ログインに失敗しました。'
+  }
 }
