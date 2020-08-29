c161a301dea128824c419db304dab51f0562e419
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-29 23:29:43 +0900

    add: store input user form valid

--- client/src/components/templates/LoginUserForm/LoginUserForm.tsx
+++ client/src/components/templates/LoginUserForm/LoginUserForm.tsx
@@ -6,9 +6,28 @@ import {
   LoginPasswordInputForm 
 } from '../../molecules/index';
 import { LoginContent } from '../../organisms/index';
+import { useRecoilValue } from 'recoil';
+import {
+  loginUserName,
+  loginUserPassword
+} from '../../../store/atoms/login';
 import './LoginUserForm.css';
 
+const { useState } = React;
+
 export function LoginUserForm() {
+  const [loginNameValid, setLoginNameValid] = useState<boolean>()
+  const [loginPasswordValid, setLoginPasswordValid] = useState<boolean>()
+  const [nameErrorMessage, setNameErrorMessage] = useState<string>('');
+  const [passwordErrorMessage, setPasswordErrorMessage] = useState<string>('');
+
+  const name = useRecoilValue(loginUserName);
+  const password = useRecoilValue(loginUserPassword);
+
+  const loginApp = () => {
+    
+  }
+
   return (
     <div className='login-content'>
       <LoginContent />
@@ -17,7 +36,7 @@ export function LoginUserForm() {
         <LoginPasswordInputForm />
       </form>
       <Link to='/'>
-        <button className='login-content-button'>
+        <button className='login-content-button' onClick={show}>
           <LoginButtonText />
         </button>
       </Link>
