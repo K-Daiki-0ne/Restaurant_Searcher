f1098094fec65d7eb87d28e5cf3c32f5a808cc4e
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-29 17:48:10 +0900

    add: store login user name

--- client/src/components/molecules/LoginNameInputForm/LoginNameInputForm.tsx
+++ client/src/components/molecules/LoginNameInputForm/LoginNameInputForm.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import { loginUserName } from '../../../store/atoms/login';
+import { useRecoilState } from 'recoil';
+
 import './LoginNameInputForm.css';
 
 export function LoginNameInputForm() {
+  const [loginName, setLoginName] = useRecoilState(loginUserName)
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLoginName(event.target.value)
+  }
+
   return (
     <div>
       <label className='name-label'>Username</label>
       <input 
         type="text" 
         className="username"  
-        placeholder="お名前を入力してください"  
+        placeholder="お名前を入力してください"
+        value={loginName}
+        onChange={onChange}
         required
       />
     </div>
