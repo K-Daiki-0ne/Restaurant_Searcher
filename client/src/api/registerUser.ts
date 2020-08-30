17791d51bd6c4366e857f3f0cc392e9f236fed2d
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-30 11:45:17 +0900

    create: post register user information

---
+++ client/src/api/registerUser.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export const registerUser = async (name: string, password: string) => {
+  try {
+    const baseUrl = process.env.REACT_APP_BACKEND_URL;
+    const reqUrl = `${baseUrl}/register/user/${name}/${password}`;
+    const result = await axios.post(reqUrl);
+    return result
+  } catch (err) {
+    
+  }
+}
