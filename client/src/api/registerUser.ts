5af91cb0c16c0a97ae309cf066f54f77e6f25272
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-08-30 21:13:29 +0900

    fix: return data

--- client/src/api/registerUser.ts
+++ client/src/api/registerUser.ts
@@ -5,7 +5,7 @@ export const registerUser = async (name: string, password: string) => {
     const baseUrl = process.env.REACT_APP_BACKEND_URL;
     const reqUrl = `${baseUrl}/register/user/${name}/${password}`;
     const result = await axios.post(reqUrl);
-    return result
+    return result.data
   } catch (err) {
     return 'ユーザーの登録に失敗しました'
   }
