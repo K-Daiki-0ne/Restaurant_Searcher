723f584fde7137859490d5355266c236467a0cde
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-09-09 22:20:30 +0900

    create: restaurant pr area for DetailRestaurantCard

--- client/src/components/templates/DetailRestaurantCard/DetailRestaurantCard.tsx
+++ client/src/components/templates/DetailRestaurantCard/DetailRestaurantCard.tsx
@@ -8,8 +8,13 @@ export const DetailRestrauntCard: React.FC = () => {
         <h1 className='detail-name'>お店の名前</h1>
       </div>
       <div className='detail-rest-grid-content'>
-        <div className='detail-image'>
-          aaa
+        <div className='detail-rest-low-grid-left-content'>
+          <div className='detail-image'>
+            aaa
+          </div>
+          <div className='detail-pr'>
+            ddd
+          </div>
         </div>
         <div className='detail-rest-low-grid-content'>
           <div className='detail-adress'>
