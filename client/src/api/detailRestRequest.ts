fd3f5ee833997fe0af6ce6cec63d822f0bb4c700
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-09-11 05:41:00 +0900

    add: get detail restaurants

--- client/src/api/detailRestRequest.ts
+++ client/src/api/detailRestRequest.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-export const detailRestRequest = async (id: string) => {
+export const detailRestRequest = async (id: string | null) => {
   const baseUrl = process.env.REACT_APP_BACKEND_URL;
   const reqURL = `${baseUrl}/api/v1/${id}`;
   try {
     const response = await axios.get(reqURL);
     console.log('API Request ...OK');
-    return response.data;
+    return response.data.rest;
   } catch (err) {
     console.log('API Request ...NO');
   }
--- client/src/pages/Detail/Detail.tsx
+++ client/src/pages/Detail/Detail.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { AppHeaer } from '../../components/organisms/index';
 import { DetailRestrauntCard } from '../../components/templates/index';
 import { useQuery } from '../../hook/useQuery';
+import { userCurrentDetailRestaurant } from '../../store/atoms/detailRestaurant';
+import { useRecoilState } from 'recoil';
+import { detailRestRequest } from '../../api/detailRestRequest';
 import './Detail.css';
 
 export function Detail() {
+  const [,setDetailRestaurant] = useRecoilState(userCurrentDetailRestaurant);
   const query = useQuery();
 
-  console.log(query.get("id"));
+  const id: string | null = query.get("id");
+
+  useEffect(() => {
+    detailRestaurantRequest()
+      .then((result: any) => setDetailRestaurant(result))
+  }, [])
+
+  const detailRestaurantRequest = async () => {
+    const result: any = await detailRestRequest(id);
+    console.log(JSON.stringify(result))
+    var rv: any = {};
+    for (var i = 0; i < result.length; ++i)
+    rv[i] = result[i];
+    console.log(rv[0])
+    return result
+  }
 
   return (
     <div className='detail'>
