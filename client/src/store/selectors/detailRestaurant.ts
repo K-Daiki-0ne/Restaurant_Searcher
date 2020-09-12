370772e8fc18ddc20cff92391460721954b59cfc
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-09-12 11:29:52 +0900

    create: detail Resturant selector

--- client/src/store/selectors/detailRestaurant.ts
+++ client/src/store/selectors/detailRestaurant.ts
@@ -1,10 +1,34 @@
-import { selectorFamily } from 'recoil';
-import { userCurrentRestaurants } from '../atoms/restraunt';
+import { selector } from 'recoil';
+import { userCurrentDetailRestaurant } from '../atoms/detailRestaurant';
 
-export const detailCurrentRestaurants = selectorFamily({
-  key: 'detailCurrentRestaurants',
-  get: restId => async({ get }) => {
-    const restList = get(userCurrentRestaurants)
-    const result: any = await restList.filter((item) => item.restId)
+export const detailCurrentRestaurantName = selector({
+  key: 'detailCurrentRestaurantName',
+  get: ({ get }) => {
+    const detailRestName: any = get(userCurrentDetailRestaurant);
+    return detailRestName.name
+  }
+})
+
+export const detailCurrentRestaurantAdress = selector({
+  key: 'detailCurrentRestaurantAdress',
+  get: ({ get }) => {
+    const detailRestAdress = get(userCurrentDetailRestaurant);
+    return detailRestAdress.address
+  }
+})
+
+export const detailCurrentRestaurantImage = selector({
+  key: 'detailCurrentRestaurantImage',
+  get: ({ get }) => {
+    const detailRestImage = get(userCurrentDetailRestaurant);
+    return detailRestImage.image_url
+  }
+})
+
+export const detailCurrentRestaurantPr = selector({
+  key: 'detailCurrentRestaurantPr',
+  get: ({ get }) => {
+    const detailRestPr = get(userCurrentDetailRestaurant);
+    return detailRestPr.pr
   }
 })
