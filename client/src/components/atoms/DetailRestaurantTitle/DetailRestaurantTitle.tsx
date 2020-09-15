b87fb3c9547ec746a281f1437a8aa0cb584de744
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-09-15 22:53:40 +0900

    add: detail restaurant title sty;e

--- client/src/components/atoms/DetailRestaurantTitle/DetailRestaurantTitle.tsx
+++ client/src/components/atoms/DetailRestaurantTitle/DetailRestaurantTitle.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useRecoilValue } from 'recoil'
 import { detailCurrentRestaurantName } from '../../../store/selectors/detailRestaurant';
+import './DetailRestaurantTitle.css';
 
 export const DetailRestaurantTitle = () => {
   const detailRestName = useRecoilValue(detailCurrentRestaurantName);
   return (
-    <div>
+    <div className='detail-restaurant-title'>
       <p>{ detailRestName }</p>
     </div>
   )
