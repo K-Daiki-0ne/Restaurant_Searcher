2dca634aef5c59ac4e3b226337898ccfbf9aeefd
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-09-15 23:00:46 +0900

    add: DetailImageCard component

--- client/src/components/organisms/DetailImageCard/DetailImageCard.tsx
+++ client/src/components/organisms/DetailImageCard/DetailImageCard.tsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-export function DetailRestaurantAdressText() {
-  return (
-    <p>DetailRestaurantAdressText</p>
+type DetailRestaurantImage = {
+  qrcode: string;
+  shop_image1: string;
+  shop_image2: string;
+}
+
+type DetailRestaurantImageProps = {
+  images: DetailRestaurantImage
+}
+
+export const DetailImageCard: React.FC<DetailRestaurantImageProps> = ({ images }): JSX.Element => {
+  const [isImage, setImage] = useState<boolean>(false);
+
+  useEffect(() => {
+    if(images.shop_image1) {
+      setImage(true);
+    } else {
+      setImage(false);
+    }
+  }, [images.shop_image1])
+
+  return isImage ? (
+    <div>
+      <img src={images.shop_image1} className='restaurant-image' />
+    </div>
+  ) : (
+    <div>
+      <img src={require('./noimage.png')} className='restaurant-image' />
+    </div>
   )
 }
