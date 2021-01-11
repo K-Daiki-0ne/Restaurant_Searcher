e84b383bb6bcd1859d93cc3f29792340b8ac8c1d
_ 0ne <kouguchi3456@yahoo.co.jp>
2021-01-11 23:45:54 +0900

    modified: List page UI

--- client/src/components/templates/ListRestaurantCard/ListRestaurantCard.tsx
+++ client/src/components/templates/ListRestaurantCard/ListRestaurantCard.tsx
@@ -4,7 +4,6 @@ import {
   ListAdressCard,
   ListImageCard 
 } from '../../organisms/index';
-// import { userCurrentRestaurants } from '../../../store/atoms/restraunt';
 import { Link } from 'react-router-dom';
 import './ListRestaurantCard.css';
 
@@ -13,24 +12,30 @@ type ListProps = {
 }
 
 export const ListRestaurantCard: React.FC<ListProps> = ({ restaurant }) => {
+  const image = {
+    qrcode: 'sample',
+    shop_image1: 'http://res.cloudinary.com/jenelle-miller/image/upload/v1518632582/tupelo-cafe_a7buc9.png',
+    shop_image2: 'shop_image2'
+  };
   return (
-    <div className='list-restaurant-card'>
+    <div className='featured-listings'>
       <Link to={`/detail?id=${restaurant.id}`} className='link'>
-        <div className="card-section">
-          <div className="card-body">
-            <div className='card-img-top'>
-              <ListImageCard images={restaurant.image_url} />
-            </div>
-            <div className="card-title">
-              <ListTitle name={restaurant.name} />
-            </div>
-            <div className="border-line"></div>
-            <p className="card-text">
-              {restaurant.address}
-              {restaurant.opentime}
-            </p>
-          </div>
+      <div className='restaurant-card'>
+        <div className='card-header'>
+          <ListImageCard images={image} />
+          <button className='favorite-btn'>
+            <i className='fas fa-heart'></i>
+          </button>
         </div>
+        <div className='card-body'>
+          <ListTitle name={restaurant.name} />
+          <div className="border-line"></div>
+          <p className="card-text">
+            {restaurant.address}
+            {restaurant.opentime}
+          </p>
+        </div>
+      </div>
       </Link>
     </div>
     )
